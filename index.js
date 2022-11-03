7672f9bcf0c1a14b448547b158a56fc8a3881085
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2022-11-03 10:53:32 +0000

    Added interativity

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const rates = document.querySelectorAll(".rate");
 const rate1 = document.getElementById("rate1");
 const rate2 = document.getElementById("rate2");
 const rate3 = document.getElementById("rate3");
@@ -37,7 +36,6 @@ rate2.addEventListener("click", (e) => {
   } else {
     rate2.classList.toggle("clicked");
   }
-  //   rate2.classList.toggle("clicked");
 });
 
 rate3.addEventListener("click", (e) => {
@@ -55,7 +53,6 @@ rate3.addEventListener("click", (e) => {
   } else {
     rate3.classList.toggle("clicked");
   }
-  //   rate3.classList.toggle("clicked");
 });
 
 rate4.addEventListener("click", (e) => {
@@ -73,7 +70,6 @@ rate4.addEventListener("click", (e) => {
   } else {
     rate4.classList.toggle("clicked");
   }
-  //   rate4.classList.toggle("clicked");
 });
 
 rate5.addEventListener("click", (e) => {
@@ -91,17 +87,49 @@ rate5.addEventListener("click", (e) => {
   } else {
     rate5.classList.toggle("clicked");
   }
-  //   rate5.classList.toggle("clicked");
 });
 
-// const rates = document.querySelectorAll(".rate");
+const rates = document.querySelectorAll(".rate");
+const rateCard = document.getElementById("rate__card");
+const selected = document.getElementById("selected");
+const thankYouCard = document.getElementById("thank__you__card");
+const submitBtn = document.getElementById("btn");
 
-// rates.forEach((rate) => {
-//   rate.addEventListener("click", () => {
-//     if (rate.classList.contains("clicked")) {
-//       rate.classList.add("changeColor");
-//     } else {
-//       rate.classList.remove("changeColor");
-//     }
-//   });
-// });
+submitBtn.addEventListener("click", (e) => {
+  if (
+    rate1.classList.contains("clicked") ||
+    rate2.classList.contains("clicked") ||
+    rate3.classList.contains("clicked") ||
+    rate4.classList.contains("clicked") ||
+    rate5.classList.contains("clicked")
+  ) {
+    for (let rate = 0; rate < rates.length; rate++) {
+      if (rates[rate].classList.contains("clicked")) {
+        switch (rate) {
+          case 0:
+            selected.innerText = 1;
+            break;
+          case 1:
+            selected.innerText = 2;
+            break;
+          case 2:
+            selected.innerText = 3;
+            break;
+          case 3:
+            selected.innerText = 4;
+            break;
+          case 4:
+            selected.innerText = 5;
+            break;
+
+          default:
+            break;
+        }
+      }
+      // console.log(result);
+    }
+    console.log("it worked");
+    rateCard.style.display = "none";
+    thankYouCard.style.display = "block";
+  }
+});
